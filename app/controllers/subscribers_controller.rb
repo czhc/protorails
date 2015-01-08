cc0938e1689ce653e064232fe85deceb9f02653e
Cedric ZH <cedrizhchan@gmail.com>
2015-01-08 15:31:53 +0800

    guest subscribe response

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,12 +1,27 @@
 class SubscribersController < ApplicationController
+  include ActionView::Helpers::UrlHelper
   layout false
 
   def create
     @subscriber = Subscriber.new(subscriber_params)
     if @subscriber.save
-      render 'create'
+      respond_to do |format|
+        format.js do 
+          flash.now[:notice] = "Success! Check your email inbox soon."
+          render 'create'
+        end
+      end
     else
-      render 'new'
+      respond_to do |format|
+        format.js do
+          if @subscriber.errors.get(:email).try(:include?, "has already been taken")
+            flash.now[:error] = "You have already subscribed. Please check your inbox for an activation email."
+          else
+            flash.now[:error] = "Invalid email address."
+          end
+          render 'error'
+        end
+      end
     end
   end
 
@@ -26,10 +41,10 @@ class SubscribersController < ApplicationController
     @subscriber = Subscriber.find_by_email(email)
     if @subscriber
       unless @subscriber.unsubscribe!
-        flash[:error] = "Whoops, something went wrong."
+        flash.now[:error] = "Whoops, something went wrong."
       end
     else
-      flash[:error] = "Invalid subscriber."
+      flash.now[:error] = "Invalid subscriber."
     end
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     Rails.logger.info "ERROR: InvalidSignature for #{params[:auth_token]}"
