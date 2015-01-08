7c272d6c08e7ba79f50051afaa730b96bf562dbe
Cedric ZH <cedrizhchan@gmail.com>
2015-01-08 14:59:36 +0800

    clear action after render

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
     @user = current_user || User.new(email: session[:user_action] == "signup" ? session[:user_email] : nil)
+    render
+    sanitize_user_form  
   end
 end
