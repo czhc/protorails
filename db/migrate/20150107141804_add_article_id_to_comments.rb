922fa40a5cee45372fb4b2920496865d9ebdfbf6
Cedric ZH Chan <cedriczhchan@gmail.com>
2015-01-09 14:39:25 +0800

    escape invalid migration

--- db/migrate/20150107141804_add_article_id_to_comments.rb
+++ db/migrate/20150107141804_add_article_id_to_comments.rb
@@ -1,6 +1,6 @@
 class AddArticleIdToComments < ActiveRecord::Migration
   def change
-    add_reference :comments, :article, index: true
-    add_foreign_key :comments, :articles
+    add_reference :comments, :article, index: true if table_exists? :comments
+    add_foreign_key :comments, :articles if table_exists? :comments
   end
 end
