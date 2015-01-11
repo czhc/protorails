a79c24bf837f20f36f1bee4838d8ce5872106aa4
Cedric ZH Chan <cedriczhchan@gmail.com>
2015-01-11 18:01:02 +0800

    add release notes?

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
-server 'server.name', user: 'deploy', roles: %w{web app db job}
+server 'protorails-production', user: 'deploy', roles: %w{web app db job}
 set :rails_env, 'production'
+set :release_note_url, 'http://prototypeB.com/release_note.txt'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,3 @@
-server 'server.name', user: 'deploy', roles: %w{web app db job}
+server 'protorails-staging', user: 'deploy', roles: %w{web app db job}
 set :rails_env, 'staging'
+set :release_note_url, 'http://staging.prototypeB.com/release_note.txt'
